1b738c274a3675dfd5fb6d4f28f438998fe798b2
Stephani-Guardado <54342733+Stephani-Guardado@users.noreply.github.com>
2020-05-10 17:07:51 -0600

    variables de sesion

--- Logueo/src/main/java/com/estefany/controladorDatos/serveletUser.java
+++ Logueo/src/main/java/com/estefany/controladorDatos/serveletUser.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.estefany.DAO.loginDAO;
 import com.estefany.model.Logueo;
@@ -30,46 +31,6 @@ public class serveletUser extends HttpServlet {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
-		Logueo o = new Logueo();
-		loginDAO dao = new loginDAO();
-		
-		String id= null;
-		String nombre= null;
-		String apellido = null;
-		String correo = null;
-		String usuario = null;
-		String contra = null;
-		
-		try {
-			
-			nombre= request.getParameter("Nombre");
-			apellido= request.getParameter("Nombres");
-			correo= request.getParameter("cUsuario");
-			usuario= request.getParameter("Usuario");
-			contra= request.getParameter("Registra una contraseÃ±a");
-				
-			
-			o.setIdlogueo(Integer.parseInt(id));
-			o.setNombres_Usuario(nombre);
-			o.setApellidos_Usuario(apellido);
-			o.setCorre_Usuario(correo);
-			o.setUsuario(usuario);
-			o.setPassword(contra);
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-		String acction = request.getParameter("btn");
-		if (acction.equals("GUARDAR")) {
-			o.setIdlogueo(Integer.parseInt(id));
-			o.setNombres_Usuario(nombre);
-			o.setApellidos_Usuario(apellido);
-			o.setCorre_Usuario(correo);
-			o.setUsuario(usuario);
-			o.setPassword(contra);
-			dao.agregarDatos(o);
-			
-		}
-		response.sendRedirect("index.jsp");
 	}
 
 	/**
@@ -81,6 +42,18 @@ public class serveletUser extends HttpServlet {
 		
 		String use = request.getParameter("usuario");
 		String contrase = request.getParameter("contra");
+		String cerrarsesion = request.getParameter("btncerrar");
+		
+		if (cerrarsesion !=null) {
+			if (cerrarsesion.equals("CERRAR")) {
+				
+				HttpSession cerrarsesiones = (HttpSession) request.getSession();
+				cerrarsesiones.invalidate();
+				
+				response.sendRedirect("index.jsp");
+			}
+		}
+		else {
 		
 		Logueo user = new Logueo();
 		loginDAO lDao = new loginDAO();
@@ -91,13 +64,14 @@ public class serveletUser extends HttpServlet {
 		int ingresarDatos =lDao.ingresarUser(user).size();
 
 		if (ingresarDatos==1) {
-			
-			System.out.println("bienvenido");
+			HttpSession seccion = request.getSession(true);
+			seccion.setAttribute("usuario", use);
+			response.sendRedirect("Principal.jsp");
 		
 		} else {
 			System.out.println("error");
 		}
-		response.sendRedirect("hola.jsp");
+		}
 	}
 
 }
---
+++ Logueo/src/main/webapp/Principal.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Principal</title>
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+
+<%
+HttpSession seccion =(HttpSession) request.getSession();
+String variabledeseccion = (String)seccion.getAttribute("usuario");
+
+if(variabledeseccion==null){
+	response.sendRedirect("index.jsp");
+}
+%>
+<body >
+<form action="serveletUser" method="post">
+<input type="submit" value="CERRAR" name="btncerrar">
+</form>
+<h1><b><i class="fas fa-clipboard">Hola Bienvenido</i></b></h1>
+</body>
+</html>
